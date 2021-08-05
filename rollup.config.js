42f14f077340ebd7067475ba23afb401665f7f79
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-08-05 13:07:16 +0200

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,27 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import vue from "rollup-plugin-vue";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import vue from 'rollup-plugin-vue'
 
-import packageJson from "./package.json";
+import packageJson from './package.json'
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
-      format: "cjs",
+      format: 'cjs',
       file: packageJson.main,
       sourcemap: true,
+      globals: { vue: 'Vue' }
     },
     {
-      format: "esm",
+      format: 'esm',
       file: packageJson.module,
       sourcemap: true,
-    },
+      globals: { vue: 'Vue' }
+    }
   ],
-  plugins: [vue(), commonjs(), typescript(), peerDepsExternal(), resolve()],
-};
+  plugins: [peerDepsExternal(), resolve(), typescript(), vue(), commonjs()],
+  external: ['vue']
+}
