9eeb132cb31bab9619a0ef2604d40331274bf60b
ricardo <ricardo@inuits.eu>
2021-08-12 09:28:27 +0200

    Updated package-lock

---
+++ lib/components/pageTitle/PageTitle.vue.d.ts
@@ -0,0 +1,19 @@
+declare const _default: import("vue").DefineComponent<{
+    title: {
+        required: true;
+        type: StringConstructor;
+    };
+    homeUrl: {
+        required: true;
+        type: StringConstructor;
+    };
+}, {
+    home: (url: any) => string;
+}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
+    title?: unknown;
+    homeUrl?: unknown;
+} & {
+    title: string;
+    homeUrl: string;
+} & {}> & {}, {}>;
+export default _default;
---
+++ lib/components/required/Required.vue.d.ts
@@ -0,0 +1,14 @@
+declare const _default: import("vue").DefineComponent<{
+    rules: {
+        type: (ObjectConstructor | StringConstructor | FunctionConstructor)[];
+        default: string;
+        required: false;
+    };
+}, unknown, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
+    rules?: unknown;
+} & {
+    rules: string;
+} & {}> & {}, {
+    rules: string;
+}>;
+export default _default;
