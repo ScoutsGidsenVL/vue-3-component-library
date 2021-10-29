cb059df1079e0ba063e966764fa74c9e9784c00d
ricardo <ricardo@inuits.eu>
2021-10-29 19:18:15 +0200

    new package lock and rebuilding everything

--- lib/components/required/Required.vue.d.ts
+++ lib/components/required/Required.vue.d.ts
@@ -7,8 +7,8 @@ declare const _default: import("vue").DefineComponent<{
 }, unknown, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
     rules?: unknown;
 } & {
-    rules: string;
-} & {}> & {}, {
-    rules: string;
+    rules: string | Function | Record<string, any>;
+} & {}>, {
+    rules: string | Function | Record<string, any>;
 }>;
 export default _default;
